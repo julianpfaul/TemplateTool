dfbcaa44fb6073baa54cd12d6430e5e7594afebc
Julian Pfaul <144740847+julianpfaul@users.noreply.github.com>
2025-01-25 14:35:50 +0100

    Delete install-tt.sh
    install-tt.sh was just a private script for installing it locally on my machine, this shouldn't have been in the public repository.

--- install-tt.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-sudo /home/pfaul/.local/share/JetBrains/Toolbox/apps/clion/bin/cmake/linux/x64/bin/cmake --build "/home/pfaul/Documents/C++/Template Tool/1/cmake-build-debug" --target install -j 10
